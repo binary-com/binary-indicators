6d25ec683fba0a6f92474301f6f38a7c9f3aeb90
Boris Yankov <borisyankov@gmail.com>
2016-09-01 09:47:41 +0300

    Move trendline files from chart here. Configure build and tests

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # binary-indicators
 Binary.com Indicators
+
+To-Do:
+* Simple Moving Average (SMA)
+* Exponential Moving Average (EMA)
+* Bollinger Bands (BB)
+* Relative Strength Index (RSI)
+* Fibonacci 
---
+++ src/__tests__/trendLine-test.js
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import linearTrendline, { linear } from '../linearTrendline';
+
+describe('linearTrendline', () => {
+    it.skip('real', () => {
+        const result = linearTrendline([[1, 3], [2, 5], [3, 6.5]]);
+        expect(result).to.deep.equal([[0, 0], [0, 0]]);
+    });
+
+    it.skip('empty list', () => {
+        const result = linear([], []);
+        expect(result).to.deep.equal([[0, 0], [0, 0]]);
+    });
+
+    it('empty list', () => {
+        const result = linear([1], [1]);
+        expect(result).to.deep.equal([[1, 1], [1, NaN]]);
+    });
+
+    it('todo 1', () => {
+        const result = linear([1, 2, 3], [1, 2, 3]);
+        expect(result).to.deep.equal([[1, 1], [3, 3]]);
+    });
+
+    it('todo 2', () => {
+        const result = linear([3, 5, 6.5], [1, 2, 3]);
+        expect(result).to.deep.equal([[3, 1], [6.5, 2.9459459459459456]]);
+    });
+
+    it('todo 3', () => {
+        const result = linear([5, 1, 3], [1, 2, 3]);
+        expect(result).to.deep.equal([[5, 1], [3, 2]]);
+    });
+});
