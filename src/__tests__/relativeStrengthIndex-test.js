3905f641e989b7a86d7c2a774d58c98125c824f8
Amin Marashi <marashi.s.amin@gmail.com>
2016-12-13 12:15:22 +0800

    Add RSI

--- src/__tests__/relativeStrengthIndex-test.js
+++ src/__tests__/relativeStrengthIndex-test.js
@@ -1,38 +1,32 @@
-import relativeStrengthIndex from '../relativeStrengthIndex';
+import relativeStrengthIndex, { relativeStrengthIndexArray } from '../relativeStrengthIndex';
 
 describe('relativeStrengthIndex', () => {
-    it.skip('data with length less than or equal to periods is equal to 0', () => {
-        const result = relativeStrengthIndex([1], { periods: 1 });
+    it('data length equal to periods is equal to 0', () => {
+        const result = relativeStrengthIndex([4, 5, 1], { periods: 3 });
         expect(result).toEqual(0);
     });
 
-    it.skip('ascending data (all wins) is equal to 0', () => {
-        const result = relativeStrengthIndex([1, 2, 3, 4], { periods: 3 });
-        expect(result).toEqual(0);
-    });
-
-    it.skip('descending data (all losses) is equal to 100', () => {
-        const result = relativeStrengthIndex([4, 3, 2, 1], { periods: 3 });
+    it('ascending data (all wins) is equal to 0', () => {
+        const result = relativeStrengthIndex([1, 3, 3.5, 4, 6.7, 8], { periods: 3 });
         expect(result).toEqual(100);
     });
 
-    it.skip('small data set', () => {
-        const data = [1, 2, 3, 3.5, 2, 5];
-        const result = relativeStrengthIndex(data, { periods: 3 });
-        expect(result).toEqual([1, 1.5, 2.25, 3.125, 4.063]);
+    it('descending data (all losses) is equal to 100', () => {
+        const result = relativeStrengthIndex([10, 8.8, 4.3, 3.11, 3.1, 3], { periods: 3 });
+        expect(result).toEqual(0);
     });
 
-    it.skip('real world', () => {
+    it('real world', () => {
       const data = [
             44.34, 44.09, 44.15, 43.61, 44.33, 44.83, 45.10, 45.42,
             45.84, 46.08, 45.89, 46.03, 45.61, 46.28, 46.28, 46.00, 46.03, 46.41,
             46.22, 45.64, 46.21, 46.25, 45.71, 46.45, 45.78, 45.35, 44.03, 44.18,
             44.22, 44.57, 43.42, 42.66, 43.13,
         ];
-        const result = relativeStrengthIndex(data, { periods: 14 });
+        const result = relativeStrengthIndexArray(data, { periods: 14 });
         expect(result).toEqual([
-            70.53, 66.32, 66.55, 69.41, 66.36, 57.97, 62.93, 63.26, 56.06, 62.38,
-            54.71, 50.42, 39.99, 41.46, 41.87, 45.46, 37.30, 33.08, 37.77,
+          70.46, 66.25, 66.48, 69.35, 66.29, 57.92, 62.88, 63.21, 56.01, 62.34,
+          54.67, 50.39, 40.02, 41.49, 41.9, 45.5, 37.32, 33.09, 37.79,
         ]);
     });
 });
--- src/relativeStrengthIndex.js
+++ src/relativeStrengthIndex.js
@@ -1,9 +1,84 @@
+import { sequence } from 'binary-utils';
+import { takeField } from './math';
+
+type CandleField = 'open' | 'high' | 'low' | 'close';
+
 type RelativeStrengthIndexConfig = {
     periods: number,
-    field: 'open' | 'high' | 'low' | 'close',
-    levels: number[],
+    field?: CandleField,
+    pipSize?: number,
 };
 
-export default (data: Candle[], config: RelativeStrengthIndexConfig) => {
-    // TODO
+const calcGain = (q1, q2) => q2 > q1 ? q2 - q1 : 0;
+const calcLoss = (q1, q2) => q2 < q1 ? q1 - q2 : 0;
+
+const calcFirstAvgDiff = (vals, comp, periods) => {
+  let prev;
+  return vals.reduce((r, q, i) => {
+    if (i === 1) {
+      prev = r;
+    }
+    const diff = comp(prev, q);
+    prev = q;
+    return diff + (i === 1 ? 0 : r);
+  }) / periods;
 };
+
+
+const calcSecondAvgDiff = (vals, comp, periods, initAvg) => {
+  let prev;
+  if (vals.length === 1) { // There is no data to calc avg
+    return initAvg;
+  }
+  return vals.reduce((r, q, i) => {
+    if (i === 1) {
+      prev = r;
+    }
+    const diff = comp(prev, q);
+    prev = q;
+    const prevAvg = i === 1 ? initAvg : r;
+    return ((prevAvg * (periods - 1)) + diff) / periods;
+  });
+};
+
+const relativeStrengthIndex = (data: Candle[], config: RelativeStrengthIndexConfig): number => {
+    const { periods, field } = config;
+
+    if (data.length < periods) {
+        throw new Error('Periods longer than data length');
+    }
+
+    if (data.length === periods) {
+        return 0;
+    }
+
+    const vals = takeField(data.slice(0, periods + 1), field);
+    // include last element from above to calc diff
+    const restSeq = takeField(data.slice(periods, data.length), field);
+
+    const initAvgGain = calcFirstAvgDiff(vals, calcGain, periods);
+    const initAvgLoss = calcFirstAvgDiff(vals, calcLoss, periods);
+
+    const avgGain = calcSecondAvgDiff(restSeq, calcGain, periods, initAvgGain);
+    const avgLoss = calcSecondAvgDiff(restSeq, calcLoss, periods, initAvgLoss);
+
+    if (avgGain === 0) {
+      return 0;
+    } else if (avgLoss === 0) {
+      return 100;
+    }
+
+    const RS = avgGain / avgLoss;
+
+    return 100 - (100 / (1 + (RS)));
+};
+
+export const relativeStrengthIndexArray = (data: Candle[], config: RelativeStrengthIndexConfig): number[] => {
+    const { periods, pipSize = 2 } = config;
+    return sequence(data.length - periods)
+        .map((x, i) =>
+            +(relativeStrengthIndex(data.slice(0, i + periods + 1), config).toFixed(pipSize))
+        );
+};
+
+export default relativeStrengthIndex;
